b6c77624140d1e08b27a8b568eaddc42943f022f
FerJRL <frl711m@uma.es>
2022-05-26 18:53:47 +0200

    Fases 1 y 2 añadidas

--- src/test/java/org/mps/authentication/UserRegistrationTest.java
+++ src/test/java/org/mps/authentication/UserRegistrationTest.java
@@ -8,7 +8,7 @@ public class UserRegistrationTest {
 
 
     @Test
-    void WhenValidatedRegisterCorrectly() {
+    void Phase1_WhenValidatedRegisterCorrectly() {
 
         //montamos mocks
         Date date = Mockito.mock(Date.class);
@@ -19,14 +19,75 @@ public class UserRegistrationTest {
         UserRegistration urSpy = Mockito.spy(UserRegistration.class);
 
         Mockito.doReturn(CredentialValidator.ValidationStatus.VALIDATION_OK).when(urSpy).validate(date, pw, cs);
-        //Mockito.when(urSpy.validate(new CredentialValidator(date, pw, cs))).thenReturn(CredentialValidator.ValidationStatus.VALIDATION_OK);
 
         urSpy.register(date, pw, cs);
 
         //verificamos que se ha llamado al register de CredentialStore
         Mockito.verify(cs).register(date, pw);
+    }
 
+    @Test
+    void Phase1_WhenNotValidatedDontRegister() {
 
+        //montamos mocks
+        Date date = Mockito.mock(Date.class);
+        PasswordString pw = Mockito.mock(PasswordString.class);
+        CredentialStore cs = Mockito.mock(CredentialStore.class);
 
+        //montamos spy de UserRegistration
+        UserRegistration urSpy = Mockito.spy(UserRegistration.class);
+
+        Mockito.doReturn(CredentialValidator.ValidationStatus.PASSWORD_INVALID).when(urSpy).validate(date, pw, cs);
+        urSpy.register(date, pw, cs);
+
+        Mockito.doReturn(CredentialValidator.ValidationStatus.BIRTHDAY_INVALID).when(urSpy).validate(date, pw, cs);
+        urSpy.register(date, pw, cs);
+
+        Mockito.doReturn(CredentialValidator.ValidationStatus.EXISTING_CREDENTIAL).when(urSpy).validate(date, pw, cs);
+        urSpy.register(date, pw, cs);
+
+        Mockito.verify(cs, Mockito.never()).register(date, pw);
+    }
+
+    @Test
+    void Phase2_WhenValidatedRegisterCorrectly() {
+
+        UserRegistration ur = new UserRegistration();
+
+        //montamos mocks
+        Date date = Mockito.mock(Date.class);
+        Mockito.when(date.validate()).thenReturn(true);
+
+        PasswordString pw = Mockito.mock(PasswordString.class);
+        Mockito.when(pw.validate()).thenReturn(true);
+
+        CredentialStore cs = Mockito.mock(CredentialStore.class);
+        Mockito.when(cs.credentialExists(date, pw)).thenReturn(false);
+
+        ur.register(date, pw, cs);
+
+        Mockito.verify(cs).register(date, pw);
+    }
+
+    @Test
+    void Phase2_WhenNotValidatedDontRegister() {
+
+        UserRegistration ur = new UserRegistration();
+
+        //montamos mocks
+        Date date = Mockito.mock(Date.class);
+
+        PasswordString pw = Mockito.mock(PasswordString.class);
+
+        CredentialStore cs = Mockito.mock(CredentialStore.class);
+
+        //ejemplo de parámetros con los que CredentialValidator no se pondria a VALIDATION_OK
+        //asegurar Multiple Condition Coverage es trabajo de las pruebas unitarias
+        Mockito.when(date.validate()).thenReturn(false);
+        Mockito.when(pw.validate()).thenReturn(false);
+        Mockito.when(cs.credentialExists(date, pw)).thenReturn(true);
+        ur.register(date, pw, cs);
+
+        Mockito.verify(cs, Mockito.never()).register(date, pw);
     }
 }
